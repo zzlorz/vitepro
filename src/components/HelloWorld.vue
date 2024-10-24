15840a51c7e37d6c989a08e9ed5631011a735b05
zzlorz <zhuzheng.lei@163.com>
2024-10-24 05:37:32 +0000

    3d预览

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { SRGBColorSpace } from 'three';
+import { ref } from 'vue';
+import { Color } from 'three';
+
+import { useRenderLoop, TresCanvas } from '@tresjs/core';
+import { OrbitControls } from '@tresjs/cientos';
+
+import TheModel from './model.vue';
+
+</script>
+
+<template>
+  <Suspense>
+    <TresCanvas
+      clear-color="#82DBC5"
+      shadows
+      alpha
+      window-size
+      power-preference="high-performance"
+      :output-color-space="SRGBColorSpace"
+    >
+      <TresPerspectiveCamera
+        :position="[5, 5, 5]"
+        :fov="75"
+        :near="0.1"
+        :far="1000"
+      />
+      <OrbitControls />
+      <TresAmbientLight :color="0xffffff" :intensity="0.25" />
+      <TresDirectionalLight :position="[0, 8, 4]" :intensity="2" cast-shadow />
+      <Suspense>
+        <TheModel />
+      </Suspense>
+    </TresCanvas>
+  </Suspense>
+</template>
---
+++ src/components/model.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useGLTF } from '@tresjs/cientos';
+
+const { scene: model } = await useGLTF(
+  'https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/blender-cube.glb',
+// '/aaa.gltf',
+  { draco: true }
+);
+</script>
+
+<template>
+  <primitive :object="model" />
+</template>
